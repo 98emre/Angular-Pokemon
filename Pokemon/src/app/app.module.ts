28252e6010db553faa8bf758e3fdcd6f30858f40
98emre <emre-98@live.se>
2023-07-24 09:07:57 +0200

    feat: added component pager for profile

--- Pokemon/src/app/app.module.ts
+++ Pokemon/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { CataloguePageComponent } from './pages/catalogue-page/catalogue-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    ProfileComponent,
+    PokemonListComponent,
+    PokemonListItemComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule,
