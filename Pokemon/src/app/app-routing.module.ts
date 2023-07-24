89e10793817c4da2d8799fab8f826051125a5355
victorlun <victor.lundqvist@se.experis.com>
2023-07-24 09:54:55 +0200

    feat: added routing

--- Pokemon/src/app/app-routing.module.ts
+++ Pokemon/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CataloguePageComponent } from './pages/catalogue-page/catalogue-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LoginPageComponent
+  },
+  {
+    path:'catalogue',
+    component:CataloguePageComponent
+    
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
