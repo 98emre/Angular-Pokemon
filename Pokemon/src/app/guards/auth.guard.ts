1c3fbb05b5c635984f39cb355af940444b69c435
98emre <emre-98@live.se>
2023-07-24 14:44:21 +0200

    fix: removed uncessary files

--- Pokemon/src/app/guards/auth.guard.ts
+++ Pokemon/src/app/guards/auth.guard.ts
@@ -1,3 +1,6 @@
+
+
+
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
