0252b2787e81f093165b8a1295b6df5f2d24319b
98emre <emre-98@live.se>
2023-07-24 14:43:16 +0200

    feat: added guard Auth

---
+++ Pokemon/src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if(localStorage.getItem("username") === "not logged" || localStorage.getItem("username") === null || localStorage.getItem("username")?.trim() === "") {
+        return false;
+      }
+
+      return true;
+  }
+  
+}
