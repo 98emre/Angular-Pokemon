a0ffe53ef3edeb102d41bd1cdb3fedc976acf56c
98emre <emre-98@live.se>
2023-07-28 15:09:20 +0200

    fix: better style in user profile

--- Pokemon/src/app/pages/profile-page/profile-page.component.ts
+++ Pokemon/src/app/pages/profile-page/profile-page.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserService } from 'src/app/services/user.service';
-import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile-page',
